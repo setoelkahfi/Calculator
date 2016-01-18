97065da082b0249e524c05f3920207c5ef52c04c
Seto El Kahfi <setoelkahfi@gmail.com>
2016-01-18 16:08:36 +0700

    Button functionality

--- app/src/main/java/id/web/setoelkahfi/calculator/MainActivity.java
+++ app/src/main/java/id/web/setoelkahfi/calculator/MainActivity.java
@@ -1,17 +1,80 @@
 package id.web.setoelkahfi.calculator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+    private Button buttonAddition;
+    private Button buttonSubtraction;
+    private Button buttonDivision;
+    private Button buttonMultiplication;
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        buttonAddition = (Button) findViewById(R.id.buttonAddition);
+        buttonSubtraction = (Button) findViewById(R.id.buttonSubtraction);
+        buttonDivision = (Button) findViewById(R.id.buttonDivision);
+        buttonMultiplication = (Button) findViewById(R.id.buttonMultiplication);
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        buttonAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double number1 = Double.parseDouble(operand1.getText().toString());
+                double number2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = number1 + number2;
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+
+        buttonSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double number1 = Double.parseDouble(operand1.getText().toString());
+                double number2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = number1 - number2;
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double number1 = Double.parseDouble(operand1.getText().toString());
+                double number2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = number1 / number2;
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+
+        buttonMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double number1 = Double.parseDouble(operand1.getText().toString());
+                double number2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = number1 * number2;
+                textResult.setText(Double.toString(theResult));
+            }
+        });
     }
 
 
